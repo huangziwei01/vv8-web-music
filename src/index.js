4c33f7a56ad6713cd081af415426acc87644c7d3
unknown <huangziwei@trio.com>
2023-08-02 18:46:33 +0800

    项目的结构搭建以及配置初始化

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 
+import '@/assets/css/reset.css'
+
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
